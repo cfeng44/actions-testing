40fedeb80d1990b601aa97c02a6dc6658a418903
Codey Funston <cfeng44@outlook.com>
2024-12-09 10:47:33 +1100

    Create Select-Reviewer.ps1

---
+++ .github/workflows/scripts/Select-Reviewer.ps1
@@ -0,0 +1,37 @@
+# ---------------------------------------------------------
+#   File:         Select-Reviewer.ps1
+#   Author:       Codey Funston [s222250824@deakin.edu.au]
+# 
+#   Description:  Based on repository preferences, selects 
+#                 a reviewer, or whole team if no matches.
+#
+# ---------------------------------------------------------
+
+# Cross references current repositoy with team member preferences to produce a
+# a list of valid reviewers.
+function Select-Reviewer {
+    param (
+        [string]$Path
+    )
+
+    $team = @()
+    $thisRepo = "${{ github.repository}}"
+    $preferences = Get-Content -Raw $path | ConvertFrom-Json -AsHashtable
+
+    foreach ($member in $preferences.Keys) {
+        if ($preferences[$member] -eq $thisRepo) {
+            $team += $member
+        }
+    }
+
+    return $team
+}
+
+if ($MyInvocation.ScriptName -ne "") {
+    param(
+        [string]$Path
+    )
+
+    $team = Select-Reviewer -Path $path
+    return $team
+}
